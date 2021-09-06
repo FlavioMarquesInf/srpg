77005f844057ab98dd5b05406c74fe36dbd3598a
Flavio <Marques>
2021-09-05 22:45:51 -0300

    Update auth.service.ts

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 
-// import { UserLoginComponent } from '../shared/deeply/nested/user-login/user-login.component';
-
-import { UserLoginComponent } from '@srpg-shared/user-login.component';
 
 @Injectable({
   providedIn: 'root'
